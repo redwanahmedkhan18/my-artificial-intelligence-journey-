82548420bf3b48db2d83b165d87b0d9b5b63ebe9
redwanahmedkhan18 <redwan.ahmed.khan.2023@gmail.com>
2026-02-18 00:18:52 +0600

    day -11 python packages and modules has completed

---
+++ day_11/modules/main.py
@@ -0,0 +1,5 @@
+import calculator
+
+print(calculator.add(10,50))
+
+print(calculator.PI)
---
+++ day_11/op.py
@@ -0,0 +1,4 @@
+from packages import math_ops, string_ops
+
+print(math_ops.multiply(4, 5))  # 20
+print(string_ops.reverse("Redwan"))  # "nawdeR"
